d8293bf99b75ab381b9661009b1d5c9509cc4087
dbykov <79261363776@yandex.ru>
2019-07-04 21:43:39 +0300

    add new test

---
+++ javatest/src/test/java/JavaTestExample1/loginLocalHost.java
@@ -0,0 +1,39 @@
+package JavaTestExample1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class loginLocalHost {
+    private static WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+
+    }
+
+    @Test
+    public void FirstTest() {
+        driver.get("http://localhost/litecart/admin/.");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
